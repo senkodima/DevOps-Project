c394bd2a3ce1c0d22c9b0266268ca692ea4e8185
Dmitry Senko <senkodima22@gmail.com>
2022-12-24 19:39:55 +0300

    update

--- files/application/cylinderTest.py
+++ files/application/cylinderTest.py
@@ -1,27 +1,26 @@
 import unittest
 import cylinder
 
-
 class cylinderTest(unittest.TestCase):
 
-    # passing tests
+    #passing tests
     def test_volume1(self):
-        assert (cylinder.volume(10, 32) >= 10053.096491487338)
+        assert(cylinder.volume(10,32) >= 10053.096491487338)
 
     def test_volume2(self):
-        assert (cylinder.volume(10, 100) >= 31415.926535897932)
-
+        assert(cylinder.volume(10,100) >= 31415.926535897932)
+    
     def test_area(self):
-        assert (cylinder.surfaceArea(5, 4) >= 282)
-
+        assert(cylinder.surfaceArea(5,4) >= 282)
+    
     def test_lateral(self):
-        assert (cylinder.lateral(5, 4) >= 125)
-
+        assert(cylinder.lateral(5,4) >= 125)
+    
     def test_base(self):
-        assert (cylinder.base(5) >= 78)
+        assert(cylinder.base(5) >= 78)
 
-    # failing test
-    # def test_volume3(self):
+    #failing test
+    #def test_volume3(self):
         #assert(cylinder.volume(10,1000) == 0)
 
 
